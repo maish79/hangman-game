3706d0312c8b3b2ffff6bb966ba28ef0b486fabe
maish79 <maishsupa@yahoo.com>
2023-03-20 10:56:46 +0000

    created word_list variable with a list of animals for the game

---
+++ hangman_letters.py
@@ -0,0 +1,7 @@
+""" guess words for hangman game """
+
+word_list = [
+    'monkey', 'python', 'gazelle', 'impala', 'toad', 'frog', 'mouse',
+    'lizard', 'rhino', 'bufallo', 'crocodile', 'tiger', 'donkey',
+    'antelope', 'eagle', 'parrot', 'mosquito', 'elephant'
+]
