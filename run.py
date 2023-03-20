360d160e10b228a88dde2b92360df82b4596e799
maish79 <maishsupa@yahoo.com>
2023-03-20 11:24:50 +0000

    created a function get_player_name which prompts the user to enter their name and returns the name as a string

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+importing modules for hangman game
+
+"""
+
+import random
+import pyfiglet
+import hangman_letters
+
+
+def display_logo():
+    """ Displays the hangman logo """
+    logo = pyfiglet.figlet_format("welcome to\n Hangman",
+                                  font="standard", justify="center")
+    print(logo)
+
+
+def get_player_name():
+    """Prompts the player to enter their name and returns the name"""
+    player_name = input("Please enter your name: ")
+    return player_name
+
+
+name = get_player_name()
+print("Hello, " + name + "! Welcome to the game")
+
